667da77a69560d9f9a6fc2f6c09832539d61afd3
Davyn <Davyn@142.207.182.205>
2015-03-02 16:50:58 -0600

    PreChange to while loop

--- src/supermarket/Cart.java
+++ src/supermarket/Cart.java
@@ -5,10 +5,57 @@
  */
 package supermarket;
 
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.*;
+
 /**
  *
  * @author Davyn
  */
 public class Cart {
+   
+
+    
+    private final Stack<Item> bills;
+    private ArrayList<Item>     cartArray = new ArrayList<Item>();
+    private ArrayList<Integer>  numArray = new ArrayList<Integer>();
+    public String myName;
+    
+    public Cart(String myName){
+        this.myName = myName;
+        bills = new Stack();
+    }
+    
+    public void addItem(Item item){
+        boolean alreadyIn = false;
+        
+        for( Item anItem : cartArray){
+           if( anItem.equals(item) ){
+               alreadyIn = true;
+           }
+        }
+        
+        if(alreadyIn){
+        
+        }else{
+            cartArray.add(item);
+        }
+    }
+    
+    public boolean detectDuplication(){
+        return true;
+    }
+    
+    public void removeItem(){
+        cartArray.remove(0);
+        numArray.remove(0);
+    }
+    
+
+    
+  
+    
+   
     
 }
